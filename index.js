915bf81eeae35b681a22644e0f64af3aed07050e
leonreucher <leonreucher@yahoo.de>
2024-10-28 18:29:32 +0100

    add buffer overflow handling

--- index.js
+++ index.js
@@ -568,47 +568,52 @@ class HDMIVideoWallProcessor extends InstanceBase {
 
 		this.messageBuffer += data
 
-		const messages = this.messageBuffer.split(/\r/)
-
-		if (!this.messageBuffer.endsWith('\r')) {
-			this.messageBuffer = messages.pop()
-		} else {
+		const MAX_BUFFER_SIZE = 1024
+		if (this.messageBuffer.length > MAX_BUFFER_SIZE) {
+			this.log('warn', 'Message buffer overflow. Clearing buffer.')
 			this.messageBuffer = ''
+			return
 		}
 
-		messages.forEach((message) => {
-			if (message.trim() === '') return
+		let messages = this.messageBuffer.split(/\r/)
 
-			for (const patternKey in regexPatterns) {
-				matchAndSetVariable(patternKey, message)
-			}
+		if (messages.length > 1) {
+			const completeMessages = messages.slice(0, -1)
+			completeMessages.forEach((message) => {
+				if (message.trim() === '') return
 
-			if (message.match(regexPatterns.outputInputSource) || message.match(regexPatterns.inputOutputTarget)) {
-				this.checkFeedbacks('output_at_input')
-			}
-			if (message.match(regexPatterns.outputExtAudioSource)) {
-				this.checkFeedbacks('output_at_audio_input')
-			}
-			if (message.match(regexPatterns.panelLock)) {
-				this.checkFeedbacks('panel_lock')
-			}
-			if (message.match(regexPatterns.outputStream)) {
-				this.checkFeedbacks('output_streaming')
-			}
-			if (message.match(regexPatterns.devicePower)) {
-				this.checkFeedbacks('device_power')
-			}
-			if (message.match(regexPatterns.deviceBeep)) {
-				this.checkFeedbacks('device_beep')
-			}
-			if (message.match(regexPatterns.activePreset)) {
-				this.getStatus()
-			}
-			if (message.match(regexPatterns.telnetConnected)) {
-				this.log('info', 'Telnet connection successfull - retreiving status.')
-				this.getStatus()
-			}
-		})
+				for (const patternKey in regexPatterns) {
+					matchAndSetVariable(patternKey, message)
+				}
+
+				if (message.match(regexPatterns.outputInputSource) || message.match(regexPatterns.inputOutputTarget)) {
+					this.checkFeedbacks('output_at_input')
+				}
+				if (message.match(regexPatterns.outputExtAudioSource)) {
+					this.checkFeedbacks('output_at_audio_input')
+				}
+				if (message.match(regexPatterns.panelLock)) {
+					this.checkFeedbacks('panel_lock')
+				}
+				if (message.match(regexPatterns.outputStream)) {
+					this.checkFeedbacks('output_streaming')
+				}
+				if (message.match(regexPatterns.devicePower)) {
+					this.checkFeedbacks('device_power')
+				}
+				if (message.match(regexPatterns.deviceBeep)) {
+					this.checkFeedbacks('device_beep')
+				}
+				if (message.match(regexPatterns.activePreset)) {
+					this.getStatus()
+				}
+				if (message.match(regexPatterns.telnetConnected)) {
+					this.log('info', 'Telnet connection successful - retrieving status.')
+					this.getStatus()
+				}
+			})
+		}
+		this.messageBuffer = messages[messages.length - 1].trim()
 	}
 
 	async configUpdated(config) {
